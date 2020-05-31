e7018a3e45c56c1468690143dcc02e499b23f9a9
Gianmarco <dev@gimbo.org>
2020-05-31 09:50:39 +0200

    Sync to the latest version

--- addons/gmAggroMon/core.lua
+++ addons/gmAggroMon/core.lua
@@ -2,21 +2,30 @@
 
 local ADDON = ...
 
--- defaults saved variables
-GMAGGROMON = GMAGGROMON or {}
+-- defaults 
 
-GMAGGROMON["ACTIVE"] = GMAGGROMON["ACTIVE"] or true
-GMAGGROMON["SOUND"] = GMAGGROMON["SOUND"] or true
-GMAGGROMON["CHAT"] = GMAGGROMON["CHAT"] or false
-GMAGGROMON["SHOW"] = GMAGGROMON["SHOW"] or false
-GMAGGROMON["SNDFLE"] = GMAGGROMON["SNDFLE"] or 1
-GMAGGROMON["ENAPVP"] = GMAGGROMON["ENAPVP"] or false
+GMAGGROMON = {
+
+	ACTIVE = true,
+	SOUND = true,
+	CHAT = false,
+	SHOW = false,
+	SNDFLE = 1,
+	ENAPVP = false,
+	
+	AFPOINT = "CENTER",
+	AFRELPNT = "CENTER",
+	AFX = 0,
+	AFY = 0,
+
+}
+
 
 local prgname = "|cffffd200gmAggroMon|r"
 local string_format = string.format
 local tostring = tostring
 
-local gmAggroMon_status_old, gmAggroMon_value, gmAggroMon_status, gmAggroMon_summary
+local status_old, gmAggroMon_value
 
 -- Frame related vars
 local bgfile = "Interface\\ChatFrame\\ChatFrameBackground"
@@ -45,14 +54,14 @@ local gmAggroMon_lvl = {
     -- 3 = Unit is mobUnit's primary target, and no other unit has 100% or higher raw threat (default UI shows red indicator)
 	-- descriptions taken from: http://wowprogramming.com/docs/api/UnitThreatSituation
 	
-local gmAggroMon_color = {
+local color = {
 	{1,1,0},
 	{1,0.5,0},
 	{1,0.2,0},
 	{1,0,0},
 }
 	
-local gmAggroMon_soundfile = {
+local soundfile = {
 	"Interface\\AddOns\\gmAggroMon\\sounds\\babe.ogg",
 	"Interface\\AddOns\\gmAggroMon\\sounds\\zenham.ogg",
 	"Interface\\AddOns\\gmAggroMon\\sounds\\pluck.ogg",
@@ -62,6 +71,11 @@ local gmAggroMon_soundfile = {
 	
 	-- Sounds taken from banzai-alert: http://www.wowace.com/addons/banzai-alert/ and from LibreOffice distribution
 }
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("ADDON_LOADED")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 
 local function gmAggroMon_check()
 
@@ -77,100 +91,37 @@ local function gmAggroMon_check()
 	end
 
 	if disable == 1 then
-		frame_cleu:UnregisterEvent("UNIT_THREAT_SITUATION_UPDATE")
-		frame_cleu2:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-		frame_cleu2:UnregisterEvent("UNIT_THREAT_SITUATION_UPDATE")
+		frame:UnregisterEvent("UNIT_THREAT_SITUATION_UPDATE")
+		frame:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 	else
-		frame_cleu:RegisterEvent("UNIT_THREAT_SITUATION_UPDATE")
-		frame_cleu2:RegisterEvent("UNIT_THREAT_SITUATION_UPDATE")
-		frame_cleu2:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-	end
-		
-	return		
+		frame:RegisterEvent("UNIT_THREAT_SITUATION_UPDATE")
+		frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+	end		
 end
 	
 		
--- Frame colored based on aggro
-local gmAggroMon_AggroFrame = CreateFrame("Frame")
-gmAggroMon_AggroFrame:SetBackdrop({bgFile = bgfile, edgeFile = edgefile, edgeSize = 18, insets = {left = 5, right = 5, top = 5, bottom = 5}})
-gmAggroMon_AggroFrame:SetBackdropColor(0,0,0,0.8)
-gmAggroMon_AggroFrame:SetWidth(40)
-gmAggroMon_AggroFrame:SetHeight(40)
-gmAggroMon_AggroFrame:SetPoint("CENTER",UIParent)
-gmAggroMon_AggroFrame:EnableMouse(true)
-gmAggroMon_AggroFrame:SetMovable(true)
-gmAggroMon_AggroFrame:RegisterForDrag("LeftButton")
-gmAggroMon_AggroFrame:SetScript("OnDragStart", function(self) self:StartMoving() end)
-gmAggroMon_AggroFrame:SetScript("OnDragStop", function(self) self:StopMovingOrSizing() end)		
-gmAggroMon_AggroFrame:SetClampedToScreen(true)
-
-local gmAggroMon_AggroValue = gmAggroMon_AggroFrame:CreateFontString("gmAggroMon_AggroFrame")
-gmAggroMon_AggroValue:SetPoint("CENTER", gmAggroMon_AggroFrame,"CENTER",0, 0)
-gmAggroMon_AggroValue:SetFont(GameFontNormal:GetFont(),(framefonth))
-gmAggroMon_AggroFrame:Hide()
+-- Frame colored based on aggro -----------------------------------------------------------------------------
+local miniFrame = CreateFrame("Frame")
+miniFrame:SetBackdrop({bgFile = bgfile, edgeFile = edgefile, edgeSize = 18, insets = {left = 5, right = 5, top = 5, bottom = 5}})
+miniFrame:SetBackdropColor(0,0,0,0.8)
+miniFrame:SetWidth(40)
+miniFrame:SetHeight(40)
+miniFrame:EnableMouse(true)
+miniFrame:SetMovable(true)
+miniFrame:RegisterForDrag("LeftButton")
+miniFrame:SetScript("OnDragStart", function(self) self:StartMoving() end)
+miniFrame:SetScript("OnDragStop", function(self) 
+	self:StopMovingOrSizing()
+	GMAGGROMON["AFPOINT"], _, GMAGGROMON["AFRELPNT"], GMAGGROMON["AFX"], GMAGGROMON["AFY"] = miniFrame:GetPoint()
+end)		
+miniFrame:SetClampedToScreen(true)
+miniFrame:SetUserPlaced(true)
 
 
-	
--- Frame to parse UNIT_THREAT_SITUATION_UPDATE	
-local frame_cleu = CreateFrame("Frame", "frame_cleu", UIParent)
-frame_cleu:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-frame_cleu:RegisterEvent("PLAYER_ENTERING_WORLD")
-frame_cleu:SetScript("OnEvent", function(self, event, ...)
-
-	if event == "ZONE_CHANGED_NEW_AREA" or event == "PLAYER_ENTERING_WORLD" then
-		gmAggroMon_check()
-	end 
-	
-	gmAggroMon_status = UnitThreatSituation("player")
-	
-	-- show the colored frame
-	if GMAGGROMON["SHOW"] == true then	
-		gmAggroMon_AggroFrame:Show()		
-		if gmAggroMon_status then
-			gmAggroMon_AggroFrame:SetBackdropColor(gmAggroMon_color[gmAggroMon_status+1][1],gmAggroMon_color[gmAggroMon_status+1][2],gmAggroMon_color[gmAggroMon_status+1][3],0.8)
-		else
-			gmAggroMon_AggroFrame:SetBackdropColor(0,1,0,0.8)			
-		end					
-	else
-		gmAggroMon_AggroFrame:Hide()
-	end
-	
-	-- print on the console
-	if GMAGGROMON["CHAT"] == true then 
-		if gmAggroMon_status then 
-			DEFAULT_CHAT_FRAME:AddMessage(string_format("gmAggroMon: [threat lvl:%i] [target:%s] %s", gmAggroMon_status, gmAggroMon_value, gmAggroMon_lvl[gmAggroMon_status+1]), gmAggroMon_color[gmAggroMon_status+1][1],gmAggroMon_color[gmAggroMon_status+1][2],gmAggroMon_color[gmAggroMon_status+1][3]) 
-		else  
-			DEFAULT_CHAT_FRAME:AddMessage("gmAggroMon: [threat nil] no aggro, unit is not on any other unit's threat table.",0,1,0) 
-		end
-	end 
-	-- play the old "Aggro!" ... It miss me so much ...  ! :-)
-	if GMAGGROMON["SOUND"] == true then 
-		if gmAggroMon_status and gmAggroMon_status >= 1 and gmAggroMon_status_old == nil then
-			PlaySoundFile(gmAggroMon_soundfile[GMAGGROMON["SNDFLE"]], "Master")
-		end
-	end 		
-	-- store the actual aggro level
-	gmAggroMon_status_old = gmAggroMon_status
-end)
-	
-
--- create a second frame to register also the COMBAT_LOG to update the value gmAggroMon_value	
-local frame_cleu2 = CreateFrame("Frame", "frame_cleu2", UIParent)
-frame_cleu2:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-frame_cleu2:RegisterEvent("PLAYER_ENTERING_WORLD")
-frame_cleu2:SetScript("OnEvent", function(self, event, ...)
-
-	local _
-
-	if event == "ZONE_CHANGED_NEW_AREA" or event == "PLAYER_ENTERING_WORLD" then
-		gmAggroMon_check()
-	end 
-
-	_,_,gmAggroMon_value,_,_ = UnitDetailedThreatSituation("player", "target");
-	gmAggroMon_value = tostring(string_format("%.0f",(gmAggroMon_value or 0)))
-	gmAggroMon_AggroValue:SetText(gmAggroMon_value)
-
-end)
+local aggroValue = miniFrame:CreateFontString("miniFrame")
+aggroValue:SetPoint("CENTER", miniFrame,"CENTER",0, 0)
+aggroValue:SetFont(GameFontNormal:GetFont(),(framefonth))
+miniFrame:Hide()
 
 -- Configuration Panel -------------------------------------------------------------------------------------
 
@@ -257,26 +208,26 @@ legenda:SetScript("OnClick",
 		print(prgname .. " colors:")
 		DEFAULT_CHAT_FRAME:AddMessage("gmAggroMon: [threat nil] no aggro, unit is not on any other unit's threat table.",0,1,0) 
 		for index = 0, 3, 1 do
-			DEFAULT_CHAT_FRAME:AddMessage(string_format("gmAggroMon: [threat lvl:%i] %s", index, gmAggroMon_lvl[index+1]), gmAggroMon_color[index+1][1],gmAggroMon_color[index+1][2],gmAggroMon_color[index+1][3]) 
+			DEFAULT_CHAT_FRAME:AddMessage(string_format("gmAggroMon: [threat lvl:%i] %s", index, gmAggroMon_lvl[index+1]), color[index+1][1],color[index+1][2],color[index+1][3]) 
 		end
 	end) 
 
 local frameshow = CreateFrame("CheckButton", "$parentFRAMESHOW", options, "InterfaceOptionsCheckButtonTemplate")
 frameshow:SetPoint("TOPLEFT", subtext, "BOTTOMLEFT", -2, -170)
-frameshow.Text:SetText("Aggro Frame")
-frameshow.tooltipText = "Show the little aggro frame"
+frameshow.Text:SetText("Aggro Frame (combat only)")
+frameshow.tooltipText = "Show the little aggro frame during combat"
 frameshow:SetChecked(GMAGGROMON["SHOW"]);
 frameshow:SetScript("OnClick", function(self)
 	
 	if frameshow:GetChecked() then 
 		GMAGGROMON["SHOW"] = true
-		print(string_format("%s: aggro frame is enabled",prgname))
-		gmAggroMon_AggroFrame:SetBackdropColor(0,1,0,0.8)	
-		gmAggroMon_AggroFrame:Show()
+		print(string_format("%s: aggro frame is enabled in combat",prgname))
+		miniFrame:SetBackdropColor(0,1,0,0.8)	
+		-- miniFrame:Show()
 	else 
 		GMAGGROMON["SHOW"] = false
 		print(string_format("%s: aggro frame is disabled",prgname))
-		gmAggroMon_AggroFrame:Hide()
+		-- miniFrame:Hide()
 	end	
 	
 end)	
@@ -305,7 +256,7 @@ testsnd:SetPoint("TOPLEFT", subtext, "BOTTOMLEFT", 170, -200)
 testsnd:SetText("Play it")
 testsnd:SetScript("OnClick", 
 	function()
-		PlaySoundFile(gmAggroMon_soundfile[GMAGGROMON["SNDFLE"]], "Master")
+		PlaySoundFile(soundfile[GMAGGROMON["SNDFLE"]], "Master")
 	end) 
 	
 local sndfile = CreateFrame("Slider", "$parentSNDFILE", options, "OptionsSliderTemplate")
@@ -315,11 +266,14 @@ sndfile:SetMinMaxValues(1, 6)
 sndfile:SetValue(1)
 sndfile.low = _G[sndfile:GetName().."Low"]
 sndfile.low:SetPoint("TOPLEFT", sndfile, "BOTTOMLEFT", 0, 0)
-sndfile.low:SetText('1')
+sndfile.low:SetText("")
+sndfile.low:SetText("1")
 sndfile.high = _G[sndfile:GetName().."High"]
 sndfile.high:SetPoint("TOPRIGHT", sndfile, "BOTTOMRIGHT", 0, 0)
-sndfile.high:SetText('6')
+sndfile.high:SetText("")
+sndfile.high:SetText("6")
 sndfile.text = _G[sndfile:GetName().."Text"]
+sndfile.text:SetText("")
 sndfile.text:SetText("Sounds")
 sndfile.tooltipText = "Sound"
 sndfile.value = sndfile:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
@@ -344,48 +298,70 @@ if LibStub and LibStub("LibAboutPanel", true) then
 end
 
 
--- Frame to register the global vars at the ADDON_LOADED event.	
-local frame_al = CreateFrame("Frame", "frame_al", UIParent)
-frame_al:RegisterEvent("ADDON_LOADED")	
-frame_al:SetScript("OnEvent", function(self, event, arg1, ...)	
-	if event == "ADDON_LOADED" and arg1 == "gmAggroMon" then		
-	
-		if GMAGGROMON["SHOW"] == false then	
-			gmAggroMon_summary = "[show no] "
-			gmAggroMon_AggroFrame:Hide()
-		else
-			gmAggroMon_summary = "[show yes] "
-			gmAggroMon_AggroFrame:Show()
-		end
-		
-		if GMAGGROMON["CHAT"] == false then	
-			gmAggroMon_summary = gmAggroMon_summary .. "[print no] "
-		else
-			gmAggroMon_summary = gmAggroMon_summary .. "[print yes] "
-		end
-		
-		if GMAGGROMON["SOUND"] == false then	
-			gmAggroMon_summary = gmAggroMon_summary .. "[sound no] "
-		else
-			gmAggroMon_summary = gmAggroMon_summary .. "[sound yes] "
-		end
 
-		if GMAGGROMON["ENAPVP"] == false then	
-			gmAggroMon_summary = gmAggroMon_summary .. "[pvp no] "
-		else
-			gmAggroMon_summary = gmAggroMon_summary .. "[pvp yes] "
-		end
-		
+frame:SetScript("OnEvent", function(self, event, arg1, ...)
+
+	if event == "ADDON_LOADED" and arg1 == "gmAggroMon" then 
+	
 		if GMAGGROMON["ACTIVE"] == false then			
-			print(string_format("%s: |cffff0000disabled|r. Type /%s to enable", prgname, prgname))
-		else
-			print(string_format("%s: |cff228B22enabled|r %s" , prgname, gmAggroMon_summary))
+			print(string.format("%s: |cffff0000disabled|r. Type /%s to enable", prgname, prgname))
 		end 
 				
 		options.refresh()
 		self:UnregisterEvent("ADDON_LOADED")		
+
+	elseif event == "PLAYER_ENTERING_WORLD" then
+
+		miniFrame:SetPoint(GMAGGROMON["AFPOINT"],nil,GMAGGROMON["AFRELPNT"],GMAGGROMON["AFX"],GMAGGROMON["AFY"])
+		gmAggroMon_check()
+	
+	elseif event == "ZONE_CHANGED_NEW_AREA" then
+		
+		gmAggroMon_check()
+
+	elseif event == "UNIT_THREAT_SITUATION_UPDATE" or event == "COMBAT_LOG_EVENT_UNFILTERED" then
+
+		local status = UnitThreatSituation("player")
+	
+		-- show the colored frame
+		if GMAGGROMON["SHOW"] == true then	
+
+			if status then
+				miniFrame:Show()
+				miniFrame:SetBackdropColor(color[status+1][1],color[status+1][2],color[status+1][3],0.8)
+			else
+				miniFrame:Hide()
+				miniFrame:SetBackdropColor(0,1,0,0.8)			
+			end					
+		end
+		
+		-- print on the console
+		if GMAGGROMON["CHAT"] == true then 
+
+			if status then 
+				DEFAULT_CHAT_FRAME:AddMessage(string_format("gmAggroMon: [threat lvl:%i] [target:%s] %s", status, gmAggroMon_value, gmAggroMon_lvl[status+1]), color[status+1][1],color[status+1][2],color[status+1][3]) 
+			else  
+				DEFAULT_CHAT_FRAME:AddMessage("gmAggroMon: [threat nil] no aggro, unit is not on any other unit's threat table.",0,1,0) 
+			end
+		end 
+		
+		-- play the old "Aggro!" ... It miss me so much ...  ! :-)
+		if GMAGGROMON["SOUND"] == true then 
+
+			if status and status >= 1 and status_old == nil then
+				PlaySoundFile(soundfile[GMAGGROMON["SNDFLE"]], "Master")
+			end
+		end 		
+		-- store the actual aggro level
+		status_old = status
+
+		_,_,gmAggroMon_value,_,_ = UnitDetailedThreatSituation("player", "target");
+		gmAggroMon_value = tostring(string_format("%.0f",(gmAggroMon_value or 0)))
+		aggroValue:SetText(gmAggroMon_value)
+	
 	end	
-end)	
+	
+end)
 
 
 -- CMD Parse --------------------------------------------------------------------------------------------
--- addons/gmAggroMon/readme.txt
+++ addons/gmAggroMon/readme.txt
@@ -1,21 +1,41 @@
-v1.0-2016102602
+830-2020031501
+- bump toc
+
+820-2019062801
+- bump toc
+
+810-2018122201
+- bump toc
+
+801-2018081101
+- bump toc
+
+720-2017032801
+- bump toc
+
+715-2017012901
+- now aggro frame save position
+- aggro frame show only in combat (so no more green frame)
+- almost a complete rewritten (in a better way I hope :)
+
+1.0-2016102602
 -bump toc
 
-v1.0-2016070901
+1.0-2016070901
 -bump toc to 7.0
 -changed name and references into gmAggroMon
 
-v1.0-2015062401
+1.0-2015062401
 - bump toc for 6.2
 
-v1.0-2015022501
+1.0-2015022501
 - bump toc 6.1
 
-v1.0-2014110301
+1.0-2014110301
 - fix some globals vars
 - rewrite saved variables (now uses an array).
 
-v1.0-2014101501
+1.0-2014101501
 - bump toc for 6.0.x
 - rewrite most of the code for the configuration panel.
 
@@ -25,11 +45,11 @@ configuration and press here and there to set the correct settings :-)
   
 ... or simply delete [WoW Path]\WTF\Account\YOUR_ACCOUNT\SavedVariables\Aggromon.lua and restart with a fresh config.
 
-v1.0-2013091001
+1.0-2013091001
 - bump toc for 5.4
 
-v1.0-2013052101
+1.0-2013052101
 - bump toc
 
-v1.0-2013051601
+1.0-2013051601
 - initial release
